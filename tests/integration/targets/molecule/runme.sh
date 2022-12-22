edd59631fea78814bf8decdae7a177efd364915d
gardar <gardar@users.noreply.github.com>
2022-12-22 14:22:05 +0000

    test: hardcode interpreter

--- tests/integration/targets/molecule/runme.sh
+++ tests/integration/targets/molecule/runme.sh
@@ -24,11 +24,6 @@ apt -y install docker.io
 pip install molecule molecule-docker
 cd ../../../../../../../../../roles/node_exporter
 ansible --version
-ansible-doc --list -j
+#ansible-doc --list -j
 #ansible-config dump | cat
-ls /
-ls /set
-ls /set/ansible_python_interpreter/
-ls /set/ansible_python_interpreter/in/
-ls /set/ansible_python_interpreter/in/inventory
-#molecule test
+molecule test

e701758869f67c60f57c74e9bd6badc1a0f2ea1a
gardar <gardar@users.noreply.github.com>
2022-12-23 14:03:16 +0000

    test: using python 3.8

--- tests/integration/targets/molecule/runme.sh
+++ tests/integration/targets/molecule/runme.sh
@@ -3,32 +3,32 @@
 set -eux
 
 echo "$@"
-ls -1
-pwd
-ls ../
-ls ../../
-ls ../../../
-ls ../../../../
-ls ../../../../../
-ls ../../../../../../
-ls ../../../../../../../
-ls ../../../../../../../../
-ls ../../../../../../../../../
-ls ../../../../../../../../../../
-ls ../../../../../../../../../../../
-ls ../../../../../../../../../../../../
+#ls -1
+#pwd
+#ls ../
+#ls ../../
+#ls ../../../
+#ls ../../../../
+#ls ../../../../../
+#ls ../../../../../../
+#ls ../../../../../../../
+#ls ../../../../../../../../
+#ls ../../../../../../../../../
+#ls ../../../../../../../../../../
+#ls ../../../../../../../../../../../
+#ls ../../../../../../../../../../../../
 
 
-apt -y update
-apt -y install docker.io
-pip install molecule molecule-docker
+#apt -y update
+#apt -y install docker.io
+#pip install molecule molecule-docker
 cd ../../../../../../../../../roles/node_exporter
 ansible --version
 #ansible-doc --list -j
-ls /root/ansible/lib/ansible
-ls /root
-ls /root/ansible
-ls /root/ansible/lib/ansible/executor
+#ls /root/ansible/lib/ansible
+#ls /root
+#ls /root/ansible
+#ls /root/ansible/lib/ansible/executor
 
 unset ANSIBLE_PYTHON_INTERPRETER
 unset ANSIBLE_CONNECTION_PATH
@@ -36,8 +36,8 @@ unset ANSIBLE_LIBRARY
 unset ANSIBLE_COLLECTIONS_PATH
 unset ANSIBLE_PLAYBOOK_DIR
 unset ANSIBLE_PYTHON_MODULE_RLIMIT_NOFILE
-env
+#env
 
 ansible-config dump --only-changed | cat
 
-molecule --debug test
+molecule test

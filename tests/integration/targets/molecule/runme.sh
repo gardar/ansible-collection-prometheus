5b5b8481c1ea42daaa282e65ea4788fd43ff5c02
gardar <gardar@users.noreply.github.com>
2022-12-23 18:10:32 +0000

    test: podman molecule

--- tests/integration/targets/molecule/runme.sh
+++ tests/integration/targets/molecule/runme.sh
@@ -3,25 +3,21 @@
 set -eux
 
 echo "$@"
-#ls -1
-#pwd
-#ls ../
-#ls ../../
-#ls ../../../
-#ls ../../../../
-#ls ../../../../../
-#ls ../../../../../../
-#ls ../../../../../../../
-#ls ../../../../../../../../
-#ls ../../../../../../../../../
-#ls ../../../../../../../../../../
-#ls ../../../../../../../../../../../
-#ls ../../../../../../../../../../../../
 
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_20.04/Release.key \
+	  | gpg --dearmor \
+	    | tee /etc/apt/keyrings/devel_kubic_libcontainers_stable.gpg > /dev/null
+echo \
+	  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_stable.gpg]\
+	      https://download.opensuse.org/repositories/devel:kubic:libcontainers:stable/xUbuntu_20.04/ /" \
+	        | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list > /dev/null
+apt-get update -qq
+apt-get -qq -y install podman
 
-apt -y update
-apt -y install docker.io
-pip install molecule molecule-docker
+#apt -y update
+#apt -y install docker.io
+#pip install molecule molecule-docker
 cd ../../../../../../../../../roles/node_exporter
 ansible --version
 #ansible-doc --list -j
@@ -31,13 +27,13 @@ ansible --version
 #ls /root/ansible/lib/ansible/executor
 
 unset ANSIBLE_PYTHON_INTERPRETER
-unset ANSIBLE_CONNECTION_PATH
-unset ANSIBLE_LIBRARY
-unset ANSIBLE_COLLECTIONS_PATH
-unset ANSIBLE_PLAYBOOK_DIR
-unset ANSIBLE_PYTHON_MODULE_RLIMIT_NOFILE
+#unset ANSIBLE_CONNECTION_PATH
+#unset ANSIBLE_LIBRARY
+#unset ANSIBLE_COLLECTIONS_PATH
+#unset ANSIBLE_PLAYBOOK_DIR
+#unset ANSIBLE_PYTHON_MODULE_RLIMIT_NOFILE
 #env
 
 ansible-config dump --only-changed | cat
 
-molecule test
+molecule -s podman test

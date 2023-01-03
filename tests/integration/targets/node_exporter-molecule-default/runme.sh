e09c8fd8a7fe532f6eaceee3dc66e7d1d91351c6
gardar <gardar@users.noreply.github.com>
2023-01-03 04:31:55 +0000

    add tests for all molecule scenarios

--- tests/integration/targets/node_exporter-molecule-default/runme.sh
+++ tests/integration/targets/node_exporter-molecule-default/runme.sh
@@ -11,20 +11,9 @@ apt -y install docker.io
 python -m pip install molecule molecule-docker
 
 cd $collection_root/roles/$role
+
+# Unset ansible-test variables that break molecule
 unset _ANSIBLE_COVERAGE_CONFIG
 unset ANSIBLE_PYTHON_INTERPRETER
 
 molecule test -s $scenario
-
-#if [ "${targetarray[1]}" = "molecule" ]; then
-#  role=(${targetarray[0]})
-#  scenario=(${targetarray[2]})
-#  echo ${role}
-#  echo ${scenario}
-#  $collection_root/tests/integration/targets/_molecule/molecule.sh ${role} ${scenario}
-#else
-#  echo "ERROR: Unable to recognize molecule test target"
-#  echo "Got: $targetname"
-#  echo "But directory should be named '<rolename>-molecule-<scenarioname>'"
-#  exit 126
-#fi

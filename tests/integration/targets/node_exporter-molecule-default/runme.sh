13de306d7d7db6ec57601d29943bd806ce35775c
gardar <gardar@users.noreply.github.com>
2023-01-03 03:59:13 +0000

    test: run molecule directly

--- tests/integration/targets/node_exporter-molecule-default/runme.sh
+++ tests/integration/targets/node_exporter-molecule-default/runme.sh
@@ -1,19 +1,30 @@
 #!/usr/bin/env bash
 
-ls ../
-pwd
 collection_root=$(pwd | grep -oP ".+\/ansible_collections\/\w+?\/\w+")
 targetname=${PWD##*/}
 targetarray=(${targetname//-/ })
-if [ "${targetarray[1]}" = "molecule" ]; then
-  role=(${targetarray[0]})
-  scenario=(${targetarray[2]})
-  echo ${role}
-  echo ${scenario}
-  $collection_root/tests/integration/targets/_molecule/molecule.sh ${role} ${scenario}
-else
-  echo "ERROR: Unable to recognize molecule test target"
-  echo "Got: $targetname"
-  echo "But directory should be named '<rolename>-molecule-<scenarioname>'"
-  exit 126
-fi
+role=(${targetarray[0]})
+scenario=(${targetarray[2]})
+
+apt -y update
+apt -y install docker.io
+python -m pip install molecule molecule-docker
+
+cd $collection_root/roles/$role
+unset _ANSIBLE_COVERAGE_CONFIG
+unset ANSIBLE_PYTHON_INTERPRETER
+
+molecule test -s $scenario
+
+#if [ "${targetarray[1]}" = "molecule" ]; then
+#  role=(${targetarray[0]})
+#  scenario=(${targetarray[2]})
+#  echo ${role}
+#  echo ${scenario}
+#  $collection_root/tests/integration/targets/_molecule/molecule.sh ${role} ${scenario}
+#else
+#  echo "ERROR: Unable to recognize molecule test target"
+#  echo "Got: $targetname"
+#  echo "But directory should be named '<rolename>-molecule-<scenarioname>'"
+#  exit 126
+#fi

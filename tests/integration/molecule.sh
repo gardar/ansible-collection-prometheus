7dbb7c0266c2f2a6c975115724df1d7f2ae4f560
gardar <gardar@users.noreply.github.com>
2023-01-04 19:03:14 +0000

    fix: install test requirements

--- tests/integration/molecule.sh
+++ tests/integration/molecule.sh
@@ -2,21 +2,27 @@
 
 collection_root=$(pwd | grep -oP ".+\/ansible_collections\/\w+?\/\w+")
 targetname=${PWD##*/}
-#targetarray=(${targetname//-/ })
-#role=${targetarray[0]}
-#scenario=${targetarray[2]}
 role=$(expr "$targetname" : '\w*-\(\w*\)-\w*')
+role_root="$collection_root/roles/$role"
 scenario=$(expr "$targetname" : '\w*-\w*-\(\w*\)')
 
+# Install requirements
 apt -y update
 apt -y install docker.io
+if [ -f "$role_root/test-requirements.txt"  ]; then
+	python -m pip install "$role_root/test-requirements.txt"
+fi
+if [ -f "$collection_root/test-requirements.txt"  ]; then
+	python -m pip install "$collection_root/test-requirements.txt"
+fi
 python -m pip install molecule molecule-docker
 
-cd "$collection_root/roles/$role"
 export MOLECULE_FILE=$collection_root/.config/molecule/config.yml
 
 # Unset ansible-test variables that break molecule
 unset _ANSIBLE_COVERAGE_CONFIG
 unset ANSIBLE_PYTHON_INTERPRETER
 
+# Run molecule test
+cd "$role_root"
 molecule -c "$collection_root/.config/molecule/config.yml" test -s "$scenario"

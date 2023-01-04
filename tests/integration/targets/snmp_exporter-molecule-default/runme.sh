0901cfb439db987b85b38a7fef676e8e59fe96ba
gardar <gardar@users.noreply.github.com>
2023-01-04 11:41:34 +0000

    fix: avoid using multiple instances of molecule script

--- tests/integration/targets/snmp_exporter-molecule-default/runme.sh
+++ tests/integration/targets/snmp_exporter-molecule-default/runme.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
 
 collection_root=$(pwd | grep -oP ".+\/ansible_collections\/\w+?\/\w+")
-targetname=${PWD##*/}
-targetarray=(${targetname//-/ })
-role=(${targetarray[0]})
-scenario=(${targetarray[2]})
-
-apt -y update
-apt -y install docker.io
-python -m pip install molecule molecule-docker
-
-cd $collection_root/roles/$role
-
-# Unset ansible-test variables that break molecule
-unset _ANSIBLE_COVERAGE_CONFIG
-unset ANSIBLE_PYTHON_INTERPRETER
-
-molecule test -s $scenario
+source $collection_root/tests/integration/molecule.sh 

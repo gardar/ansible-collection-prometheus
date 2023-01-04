ad5d32effe9eff896a51c4a61387c4112408234e
gardar <gardar@users.noreply.github.com>
2023-01-04 13:39:38 +0000

    fix: sanity

--- tests/integration/molecule.sh
+++ tests/integration/molecule.sh
@@ -3,18 +3,18 @@
 collection_root=$(pwd | grep -oP ".+\/ansible_collections\/\w+?\/\w+")
 targetname=${PWD##*/}
 targetarray=(${targetname//-/ })
-role=(${targetarray[0]})
-scenario=(${targetarray[2]})
+role=${targetarray[0]}
+scenario=${targetarray[2]}
 
 apt -y update
 apt -y install docker.io
 python -m pip install molecule molecule-docker
 
-cd $collection_root/roles/$role
+cd "$collection_root/roles/$role"
 export MOLECULE_FILE=$collection_root/.config/molecule/config.yml
 
 # Unset ansible-test variables that break molecule
 unset _ANSIBLE_COVERAGE_CONFIG
 unset ANSIBLE_PYTHON_INTERPRETER
 
-molecule -c $collection_root/.config/molecule/config.yml test -s $scenario
+molecule -c "$collection_root/.config/molecule/config.yml" test -s "$scenario"

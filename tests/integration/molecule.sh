9abb570bb30e98b8b052bb20a2665f8bce004377
gardar <gardar@users.noreply.github.com>
2023-01-11 12:07:12 +0000

    fix: add yamllint config

--- tests/integration/molecule.sh
+++ tests/integration/molecule.sh
@@ -18,6 +18,7 @@ fi
 python -m pip install molecule molecule-docker
 
 export MOLECULE_FILE=$collection_root/.config/molecule/config.yml
+export YAMLLINT_CONFIG_FILE=$collection_root/.config/yamllint/config.yml
 
 # Unset ansible-test variables that break molecule
 unset _ANSIBLE_COVERAGE_CONFIG

4d58a65df7bb01e1334affc99c6f7cc6e6e7fa53
gardar <gardar@users.noreply.github.com>
2023-01-11 16:42:57 +0000

    fix: lint

--- tests/integration/molecule.sh
+++ tests/integration/molecule.sh
@@ -7,16 +7,20 @@ role_root="$collection_root/roles/$role"
 scenario=$(expr "$targetname" : '\w*-\w*-\(\w*\)')
 ansible_version="$(ansible --version | head -1 | sed 's/[^0-9\.]*//g')"
 
-# Install requirements
+# Install package requirements
 apt -y update
 apt -y install docker.io
+
+# Install test requirements from role
 if [ -f "$role_root/test-requirements.txt"  ]; then
 	python -m pip install -r "$role_root/test-requirements.txt"
 fi
+# Install test requirements from collection
 if [ -f "$collection_root/test-requirements.txt"  ]; then
 	python -m pip install -r "$collection_root/test-requirements.txt"
 fi
 
+# Install ansible version specific requirements
 if [ "$(printf '%s\n' "2.12" "$ansible_version" | sort -V | head -n1)" = "2.12" ]; then 
        python -m pip install molecule molecule-plugins[docker]
 else
@@ -24,6 +28,7 @@ else
        ansible-galaxy collection install -r "$collection_root/requirements.yml"
 fi
 
+# Define config locations within collection
 export MOLECULE_FILE=$collection_root/.config/molecule/config.yml
 export YAMLLINT_CONFIG_FILE=$collection_root/.config/yamllint/config.yml
 

e5d20ee01168e126df5bce7b23056c525226c8e8
gardar <gardar@users.noreply.github.com>
2023-01-12 20:08:59 +0000

    test: fix ci
    Signed-off-by: gardar <gardar@users.noreply.github.com>

--- tests/integration/molecule.sh
+++ tests/integration/molecule.sh
@@ -25,6 +25,7 @@ if [ "$(printf '%s\n' "2.12" "$ansible_version" | sort -V | head -n1)" = "2.12" 
        python -m pip install molecule molecule-plugins[docker]
 else
        python -m pip install molecule molecule-docker
+       ansible-galaxy collection install community.docker
        ansible-galaxy collection install -r "$collection_root/requirements.yml"
 fi
 
